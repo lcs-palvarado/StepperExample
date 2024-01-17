6a66ae656424e2d9625ebf03b439abf92e3e797e
Pablo Alvarado <palvarado@lcs.on.ca>
2024-01-16 20:37:12 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Pablo Alvarado on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
